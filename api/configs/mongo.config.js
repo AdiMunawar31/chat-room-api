16c0d82bed5be2a67ad8c352d936cc71e171b450
adimunawar31 <munawar.adi31@gmail.com>
2024-10-15 15:43:09 +0700

    mongo update

--- api/configs/mongo.config.js
+++ api/configs/mongo.config.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/chat-app");
+    await mongoose.connect(
+      "mongodb+srv://d2y:8ibVFEaZRa2JgznP@cluster0.kcyvmts.mongodb.net/chat-app?retryWrites=true"
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
